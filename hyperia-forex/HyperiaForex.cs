7af42d8b68eeb778b47aa62dc592e6544f100624
amrtlt95 <amrtlt90@gmail.com>
2024-04-14 15:41:41 +0200

    Solved "Hyperia Forex"

--- hyperia-forex/HyperiaForex.cs
+++ hyperia-forex/HyperiaForex.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.CompilerServices;
+
 public struct CurrencyAmount
 {
     private decimal amount;
@@ -9,11 +12,24 @@ public struct CurrencyAmount
         this.currency = currency;
     }
 
-    // TODO: implement equality operators
+    public static bool operator ==(CurrencyAmount c1, CurrencyAmount c2) => IsCurrenciesTheSame(c1, c2) ? c1.amount == c2.amount : CurrenciesAreNotTheSame<bool>();
+    public static bool operator !=(CurrencyAmount c1, CurrencyAmount c2) => !(c1 == c2);
+    public static bool operator <(CurrencyAmount c1, CurrencyAmount c2) => IsCurrenciesTheSame(c1, c2) ? c1.amount < c2.amount: CurrenciesAreNotTheSame<bool>();
+    public static bool operator >(CurrencyAmount c1, CurrencyAmount c2) => !(c1 < c2);
+    public static decimal operator +(CurrencyAmount c1, CurrencyAmount c2) => IsCurrenciesTheSame(c1,c2) ? c1.amount+c2.amount :CurrenciesAreNotTheSame<decimal>();
+    public static decimal operator -(CurrencyAmount c1, CurrencyAmount c2) => IsCurrenciesTheSame(c1,c2) ? c1.amount -c2.amount :CurrenciesAreNotTheSame<decimal>();
+    public static decimal operator *(CurrencyAmount c1, CurrencyAmount c2) => IsCurrenciesTheSame(c1,c2) ? c1.amount * c2.amount :CurrenciesAreNotTheSame<decimal>();
+    public static decimal operator /(CurrencyAmount c1, CurrencyAmount c2) 
+    {
+        if(c2.amount != 0)
+            return IsCurrenciesTheSame(c1, c2) ? c1.amount / c2.amount : CurrenciesAreNotTheSame<decimal>();
+        return 0;
+    }
+    public static implicit operator decimal(CurrencyAmount currencyAmount) => currencyAmount.amount;
+    public static explicit operator double(CurrencyAmount currencyAmount) => (double)currencyAmount.amount;
 
-    // TODO: implement comparison operators
-
-    // TODO: implement arithmetic operators
-
-    // TODO: implement type conversion operators
+    #region Helper method
+    private static bool IsCurrenciesTheSame(CurrencyAmount c1, CurrencyAmount c2) => c1.currency == c2.currency;
+    private static T CurrenciesAreNotTheSame<T>() => throw new ArgumentException("Currencies are not the same");
+    #endregion
 }
